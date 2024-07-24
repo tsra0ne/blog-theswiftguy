9d884e3fb26fac4fca9438837b42284d3d80337a
tsra0ne <tsravangoud164@gmail.com>
2024-07-24 10:08:14 +0530

    corrected broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,10 +47,7 @@ const config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
+        }
       }),
     ],
   ],
